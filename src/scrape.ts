250ae8cc8f0db6cd534688e7067a303140af118d
quinton11 <quintons831@gmail.com>
2022-07-28 15:24:57 +0000

    Y-Comb page scraper

--- src/scrape.ts
+++ src/scrape.ts
@@ -2,6 +2,8 @@ import { gethtml } from "@service/request.serv";
 import Cheerio from "cheerio";
 import { extractTC } from "@utils/extract.tc";
 import { TCPage } from "@layout/tcrunch.page";
+import { YComb } from "@layout/ycomb.page";
+import { GenericPage } from "@typedefs/generic.type";
 
 //Pass in article to scraper
 //Scraper Returns data and logs it
@@ -9,19 +11,21 @@ import { TCPage } from "@layout/tcrunch.page";
 //Make request to site for html obj with service
 //Get html response and pass it to cheerio parsing function
 export default class Scraper {
-  tcp: TCPage;
+  tcp: GenericPage;
+  ycb: GenericPage
   constructor() {
     this.tcp = new TCPage();
+    this.ycb = new YComb();
   }
 
   public async result() {
     console.log("Fetching html...");
 
-    const res = await gethtml(this.tcp.url);
-
+    const res = await gethtml(this.ycb.url);
+    //console.log(res)
     const $ = Cheerio.load(res);
 
-    const data = this.tcp.extractData($);
+    const data = this.ycb.extractData($);
 
     console.log(data);
   }
